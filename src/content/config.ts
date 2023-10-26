32ff378f43f7413c67f12f852fde7e0ef35fb0eb
Heidi Negrete <heidi@negrete.dev>
2023-10-26 19:00:03 -0400

    update domain for sitemap creation

--- src/content/config.ts
+++ src/content/config.ts
@@ -24,13 +24,38 @@ const storeSchema = z.object({
     heroImage: z.string().optional(),
 });
 
+const projectSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    projectDates: z.string().optional(),
+    heroImage: z.string().optional(),
+    badge: z.string().optional(),
+    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
+        message: 'tags must be unique',
+    }).optional(),
+});
+
+const serviceSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    projectDates: z.string().optional(),
+    heroImage: z.string().optional(),
+    badge: z.string().optional(),
+});
+
 export type BlogSchema = z.infer<typeof blogSchema>;
 export type StoreSchema = z.infer<typeof storeSchema>;
+export type projectSchema = z.infer<typeof projectSchema>;
+export type serviceSchema = z.infer<typeof serviceSchema>;
 
 const blogCollection = defineCollection({ schema: blogSchema });
 const storeCollection = defineCollection({ schema: storeSchema });
+const projectCollection = defineCollection({ schema: projectSchema });
+const serviceCollection = defineCollection({ schema: serviceSchema });
 
 export const collections = {
     'blog': blogCollection,
-    'store': storeCollection
+    'store': storeCollection,
+    'project': projectCollection,
+    'service': serviceCollection
 }
